43a39648b848b8846b01da961e7f644d1eb2910d
delvecchio1000 <delvecchio1000@gmail.com>
2022-11-19 02:53:03 -0300

    Fazendo commit a partir do Pycharm; Edição do Perfil; Funcionalidade do Formulário de Editar Perfil; Campo de Edição de Foto de Perfil

---
+++ comunidadeimpressionadora/routes.py
@@ -0,0 +1,87 @@
+# LINKS
+# ROUTES
+
+from flask import render_template, redirect, url_for, flash, request
+from comunidadeimpressionadora import app, database, bcrypt
+from comunidadeimpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil
+from comunidadeimpressionadora.models import Usuario
+from flask_login import login_user, logout_user, current_user, login_required
+
+lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/usuarios')
+@login_required
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=form_login.lembrar_dados.data)
+            flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+            par_next = request.args.get('next')  # par_next (leia-se parâmetro next, que é a pagina que o usuário queria acessar antes do login)
+            if par_next:
+                return  redirect(par_next)
+            else:
+                return redirect(url_for('home'))
+        else:
+            flash(f'Falha no Login. E-mail ou Senha incorretos', 'alert-danger')
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        # Criptografia de senha
+        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        # Criar o usuário
+        usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_cript)
+        # Adicionar a sessão
+        database.session.add(usuario)
+        # commit na sessão
+        database.session.commit()
+        flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+
+@app.route('/sair')
+@login_required
+def sair():
+    logout_user()
+    flash(f'Logout feito com sucesso', 'alert-success')
+    return redirect(url_for('home'))
+
+@app.route('/perfil')
+@login_required
+def perfil():
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('perfil.html', foto_perfil=foto_perfil)
+
+@app.route('/post/criar')
+@login_required
+def criar_post():
+    return render_template('criarpost.html')
+
+@app.route('/perfil/editar', methods=['GET', 'POST'])
+@login_required
+def editar_perfil():
+    form = FormEditarPerfil()
+    if form.validate_on_submit():
+        current_user.email = form.email.data
+        current_user.username = form.username.data
+        database.session.commit()
+        flash(f'Perfil atualizado com sucesso', 'alert-success')
+        return redirect(url_for('perfil'))
+    elif request.method == "GET": #vai preencher automaticamente os campos Nome usuário e e-mail, na página editar perfil
+        form.email.data = current_user.email
+        form.username.data = current_user.username
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('editarperfil.html', foto_perfil=foto_perfil, form=form)
