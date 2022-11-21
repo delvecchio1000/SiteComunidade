48e992ccb4ba57e675b288870eac92e5fcb889c2
delvecchio1000 <delvecchio1000@gmail.com>
2022-11-21 19:49:39 -0300

    Aplicando funcionalidade no excluir post do modal

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -1,7 +1,7 @@
 # LINKS
 # ROUTES
 
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, abort
 from comunidadeimpressionadora import app, database, bcrypt
 from comunidadeimpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil, FormCriarPost
 from comunidadeimpressionadora.models import Usuario, Post
@@ -145,7 +145,6 @@ def exibir_post(post_id):
         if request.method == 'GET':
             form.titulo.data = post.titulo
             form.corpo.data = post.corpo
-
         elif form.validate_on_submit():
             post.titulo = form.titulo.data
             post.corpo = form.corpo.data
@@ -155,3 +154,15 @@ def exibir_post(post_id):
     else:
         form = None
     return render_template('post.html', post=post, form=form)
+
+@app.route('/post/<post_id>/excluir', methods=['GET', 'POST'])
+@login_required
+def excluir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        database.session.delete(post)
+        database.session.commit()
+        flash('Post excluido com sucesso', 'alert-danger')
+        return redirect(url_for('home'))
+    else:
+        abort(403)
